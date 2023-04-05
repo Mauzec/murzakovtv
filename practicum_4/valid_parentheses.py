aa8d434ab54e57c685f10eed2ece6bf6be9ee9a5
mauzc <49997318+Mauzec@users.noreply.github.com>
2023-04-05 19:44:42 +0300

    update(pract4): done

--- practicum_4/valid_parentheses.py
+++ practicum_4/valid_parentheses.py
@@ -1,70 +1,86 @@
 from typing import Any
 
-import yaml
 import numpy as np
 from numpy.typing import NDArray
-
-from src.common import ProblemCase
-
-
-class Stack:
-    """LIFO queue"""
-
-    def __init__(self, max_n: int, dtype: Any) -> None:
-        self._array: NDArray = np.zeros((max_n,), dtype=dtype)  # internal array
-        self._top_i: int = -1  # index of the most recently inserted element
-
-    def empty(self) -> bool:
-        return self._top_i == -1
-
-    def push(self, x: Any) -> None:
-        """Complexity: O(1)"""
-        self._top_i += 1
-        if self._top_i == len(self._array):
-            raise StackOverflowException("Stack is already full")
-        self._array[self._top_i] = x
-
-    def pop(self) -> Any:
-        """Complexity: O(1)"""
-        if self.empty():
-            raise StackUnderflowException("Stack is already empty")
-        self._top_i -= 1
-        return self._array[self._top_i + 1]
-
+import yaml
 
 class StackUnderflowException(BaseException):
-    pass
-
-
+	pass
 class StackOverflowException(BaseException):
-    pass
+	pass
 
+class LIFOStack:
+	# Initialisation
+	def __init__(self, max_n: int, dtype: Any, top_index: int=-1) -> None:
+		self._array: NDArray = np.zeros((max_n), dtype=dtype)
+		self.top_index: int = -1
+	
+	@classmethod
+	def stack_from_list(cls, array: list[Any]):
+		pass
+	
+	@classmethod
+	def stack_from_array(cls, array: NDArray[Any]):
+		pass	
+	
+	@classmethod
+	def stack_from_str(cls, s: str):
+		pass
+	
+	# Push an element into stack
+	def push(self, x: Any) -> None:
+		""" Complexity: O(1) """
+		
+		self.top_index += 1
+		
+		if (self.top_index == len(self._array)):
+			raise StackOverflowException("Stack is already full")
+		
+		self._array[self.top_index] = x
+	
+	def pop(self) -> None:
+		""" Complexity: O(1) """
+		
+		if (self.top_index == -1):
+			raise StackUnderflowException("Stack is already empty")
+		
+		self.top_index -= 1
+		return self._array[self.top_index + 1]
+	
+	def is_empty(self) -> bool:
+		return self.top_index == -1
 
-def get_starting_symbol(sym: str) -> str:
-    if sym == ")":
-        return "("
-    elif sym == "]":
-        return "["
-    elif sym == "}":
-        return "{"
-    else:
-        raise ValueError(f'Unknown parenthesis: "{sym}"')
+def get_opposite_symbol(s: str) -> str:
+	if s == "}": return '{'
 
+	if s == ")": return '('
+	
+	if s == "]": return '['
+	
 
 def are_parentheses_valid(s: str) -> bool:
-    stack = Stack(max_n=100, dtype=str)
-    stack.push(s[0])
-    starting_symbols = ["(", "{", "["]
-    #    ending_symbols = [')', '}', ']']
-    for sym in s[1:]:
-        if sym in starting_symbols:
-            stack.push(sym)
-        else:
-            last_sym = stack.pop()
-            if get_starting_symbol(sym) != last_sym:
-                return False
-    return stack.empty()
-
+	stack = LIFOStack(max_n=100, dtype=str)
+	stack.push(s[0])
+	
+	open_symbols = ["(", "[", "{"]
+	end_symbols = [")", "]", "}"]
+	
+	for x in s[1:]:
+		if x in open_symbols:
+			stack.push(x)
+		else:
+			try:
+				tmp = stack.pop()
+			except StackUnderflowException:
+				continue
+				
+			if get_opposite_symbol(x) != tmp:
+				return False
+			
+	return stack.is_empty()
+				
+			
+	
 
 if __name__ == "__main__":
     # Let's solve Valid Parentheses problem from leetcode.com:
