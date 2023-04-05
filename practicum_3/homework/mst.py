95a957c49bf7b833acb45781c44a5cce628de7c0
mauzc <49997318+Mauzec@users.noreply.github.com>
2023-04-05 21:44:05 +0300

    update(pract 3): done

--- practicum_3/homework/mst.py
+++ practicum_3/homework/mst.py
@@ -4,22 +4,40 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 from src.plotting import plot_graph
+from queue import PriorityQueue as PQ
 
 
 def prim_mst(G: nx.Graph, start_node="0") -> set[tuple[Any, Any]]:
-    mst_set = set()  # set of nodes included into MST
+    mst_set = set(start_node)  # set of nodes included into MST
     rest_set = set(G.nodes())  # set of nodes not yet included into MST
+    rest_set.remove(start_node)
+    
     mst_edges = set()  # set of edges constituting MST
-
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    
+    pq = PQ()
+    current = start_node
+    while rest_set:
+    	for neighbor in G.neighbors(current):
+    		if neighbor in rest_set:
+    			pq.put([G[current][neighbor]['weight'], current, neighbor])
+    
+    	nodeFirstDone, nodeSecDone = None, current
+    	while (current == nodeSecDone):
+    		firstNode, secNode = pq.get()[1:]
+    		if (secNode in rest_set):
+    			nodeFirstDone = firstNode
+    			nodeSecDone = secNode
+    	
+    	mst_edges.add((nodeFirstDone, nodeSecDone))
+    	current = nodeSecDone
+    	
+    	rest_set.remove(nodeSecDone)
 
     return mst_edges
 
 
 if __name__ == "__main__":
-    G = nx.read_edgelist("practicum_3/homework/graph_1.edgelist", create_using=nx.Graph)
-    plot_graph(G)
+    G = nx.read_edgelist("graph_1.edgelist", create_using=nx.Graph)
+    #plot_graph(G)
     mst_edges = prim_mst(G, start_node="0")
     plot_graph(G, highlighted_edges=list(mst_edges))
--- practicum_3/homework/sp.py
+++ practicum_3/homework/sp.py
@@ -6,19 +6,41 @@ from src.plotting import plot_graph
 
 
 def dijkstra_sp(G: nx.Graph, source_node="0") -> dict[Any, list[Any]]:
-    shortest_paths = {}  # key = destination node, value = list of intermediate nodes
+    shortest_paths = {source_node: [None, 0]} 
+    paths = {source_node: [source_node]} # key = destination node, value = list of intermediate nodes
+    
+    current = source_node
+    visited = set()
+    
+    while len(shortest_paths) < len(G.nodes()):
+    	visited.add(current)
+    	
+    	weightToCurrent = shortest_paths[current][1]
+    	
+    	for next in G.neighbors(current):
+    		weight = G[current][next]['weight'] + weightToCurrent
+    		
+    		if next not in shortest_paths:
+    			shortest_paths[next] = [current, weight]
+    			paths[next] = paths[current] + [next]
+    		else:
+    			currentShortestWeight = shortest_paths[next][1]
+    			if currentShortestWeight > weight:
+    				shortest_paths[next] = [current, weight]
+    	
+    	nextDestinat = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
+    	
+    	current = min(nextDestinat, key=lambda x: nextDestinat[x][1])
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
-
-    return shortest_paths
+    return paths
 
 
 if __name__ == "__main__":
-    G = nx.read_edgelist("practicum_3/homework/graph_1.edgelist", create_using=nx.Graph)
-    plot_graph(G)
+    G = nx.read_edgelist("graph_1.edgelist", create_using=nx.Graph)
+    #plot_graph(G)
     shortest_paths = dijkstra_sp(G, source_node="0")
+    print(shortest_paths)
+#    plot_graph(G)
     test_node = "5"
     shortest_path_edges = [
         (shortest_paths[test_node][i], shortest_paths[test_node][i + 1])
